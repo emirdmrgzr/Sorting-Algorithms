9014de9dfa55aceb90f6aa6998e56ece27a61951
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-02-15 19:45:29 +0300

    first commit

---
+++ BubbleSort/bubble.js
@@ -0,0 +1,16 @@
+const arr = [17,3,9,24,1,12,6];
+
+function bubbleSort(arr){
+  for(let i = 1;i<arr.length;i++){
+    for(let j = 0;j<arr.length-i;j++){
+      if(arr[j] > arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+}
+
+bubbleSort(arr);
